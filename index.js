3d59051ef0f91a41a5d8b89a61a0c4942084beb3
Siddharth <sid.sharma1704@gmail.com>
2022-09-29 23:26:30 +0530

    Added routing system

--- index.js
+++ index.js
@@ -6,3 +6,23 @@ let darkMode = false
 
 const tiles = ['https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png','http://{s}.tile.osm.org/{z}/{x}/{y}.png']
 L.tileLayer(darkMode ? tiles[0] : tiles[1]).addTo(map);
+
+var control = L.Routing.control(L.extend(window.lrmConfig, {
+	waypoints: [
+		L.latLng(28.6131, 77.2248),
+		L.latLng(28.6562, 77.2410)
+	],
+	geocoder: L.Control.Geocoder.nominatim(),
+	routeWhileDragging: true,
+	reverseWaypoints: true,
+	showAlternatives: true,
+	altLineOptions: {
+		styles: [
+			{color: 'black', opacity: 0.15, weight: 9},
+			{color: 'white', opacity: 0.8, weight: 6},
+			{color: 'blue', opacity: 0.5, weight: 2}
+		]
+	}
+})).addTo(map);
+
+L.Routing.errorControl(control).addTo(map);
